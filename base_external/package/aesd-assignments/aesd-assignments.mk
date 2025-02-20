77d66098276ef97afc459c848ed04981445c7d74
Ciro Donnarumma <ciro.donnarumma89@gmail.com>
2025-02-20 12:31:03 +0100

    assignment-4-complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,14 +6,13 @@
 ##############################################################
 
 #Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'f601d2e0f858f8c6561967992b225e269f81f77b'
+AESD_ASSIGNMENTS_VERSION = '89d79e3064ca6cdb90a0f012946c8bac40602386'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-CiroDonnarumma89.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
-AESD_ASSIGNMENTS_OVERRIDE_SRCDIR=/home/ciro/Projects/Coursera/EmbeddedLinux/assignment-3-and-later-CiroDonnarumma89
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
